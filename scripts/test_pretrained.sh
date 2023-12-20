ea6fb685bebee3b9992c75964b93d2f2d176bca0
KylinYee <1550130384@qq.com>
2023-12-20 19:33:02 +0800

    add postprocess in test

--- scripts/test_pretrained.sh
+++ scripts/test_pretrained.sh
@@ -11,7 +11,7 @@ python test.py \
     --aud ./pretrained/test_lm3ds.npy \
     --workspace trial_test \
     --bg_img ./pretrained/bc.jpg \
-    -O --torso --data_range 0 100 
+    -O --torso --data_range 200 300 
 
 # merge audio with video
 ffmpeg -y -i trial_test/results/ngp_ep0028.mp4 -i ./pretrained/test.wav -c:v copy -c:a aac trial_test/results/${method}_${vid}_${cond_type}_aud.mp4
